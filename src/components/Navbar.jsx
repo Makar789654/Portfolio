ffba0399bead3556e06d1227825686a6199842f7
Makar789654 <112611445+Makar789654@users.noreply.github.com>
2023-04-07 16:29:55 +0200

    12

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
   return (
     <>
       <div className="navbarcon flex justify-between items-center px-20 py-6 bg-transparent z-40 text-white lg:px-6 fixed w-full">
-        <h1 className="text-4xl  font-bold ">Reayhs</h1>
+        <h1 className="text-4xl  font-bold ">Roman Makarets</h1>
 
         <nav
           className={` flex justify-center items-center gap-x-10 ${
@@ -46,17 +46,10 @@ function Navbar() {
           </ul>
 
           <div className="icon text-lg flex gap-4  lg:gap-8 lg:my-10">
-            <a href="https://twitter.com/reayhs" target="_blank">
-              <i className="fa-brands fa-twitter border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+          <a href="https://www.linkedin.com/in/roman-makarets-35155098/" target="_blank">
+              <i className="fa-brands fa-linkedin border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
             </a>
-
-            <a href="https://www.instagram.com/iburakz/" target="_blank">
-              <i className="fa-brands fa-instagram border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
-            </a>
-            <a
-              href="https://github.com/Reayhs/react-tailwind-portfolio"
-              target="_blank"
-            >
+            <a href="https://github.com/Makar789654?tab=repositories" target="_blank">
               <i className="fa-brands fa-github border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
             </a>
           </div>
